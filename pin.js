e2f5a739a942d6a25dc5c0c3bfda696ef0be2b1b
Mark Khramov <hramov.mark321@gmail.com>
2023-03-01 22:47:10 +0300

    Create pin.js

---
+++ pin.js
@@ -0,0 +1,11 @@
+function validatePIN (pin) {
+    if (pin.length != 4 && pin.length != 6){
+      return false
+    }
+    for (i = 0; i < pin.length; i++){
+      if(pin[i] > "9" || pin[i] <"0"){
+        return false
+      }
+    }
+    return true
+  }
