3dbd3e5b8c2c47529d75a6779fd8b8e963c8a549
Mark Khramov <hramov.mark321@gmail.com>
2023-03-03 19:07:27 +0300

    Create friendOrFoe.js

---
+++ friendOrFoe.js
@@ -0,0 +1,11 @@
+function friend(friends){
+    let result = [];
+    for (let i=0; i<friends.length; i++) {
+      if (friends[i].length == 4) {
+        result.push(friends[i]);
+      }
+        
+      }
+    return result;
+  }
+  
