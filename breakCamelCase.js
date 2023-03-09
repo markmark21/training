00acae99a6ab752cc81c7f3f732768cebc5b20a7
Mark Khramov <hramov.mark321@gmail.com>
2023-03-09 16:36:31 +0300

    Create breakCamelCase.js

---
+++ breakCamelCase.js
@@ -0,0 +1,13 @@
+// complete the function
+function solution(string) {
+    var newString = "";
+  for (letter of string) {
+    if (letter == letter.toUpperCase()) {
+      newString += " ";
+      newString += letter;
+    } else {
+      newString += letter;
+    }
+  }
+  return newString;
+}
