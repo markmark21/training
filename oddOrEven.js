94e1c7953a2c9d45b497412a3882da7e287eb7fb
Mark Khramov <hramov.mark321@gmail.com>
2023-03-17 19:33:15 +0300

    Create oddOrEven.js

---
+++ oddOrEven.js
@@ -0,0 +1,12 @@
+function oddOrEven(array) {
+    let result = 0;
+   for(let i = 0; i < array.length; i++) {
+     result += array[i];
+   }
+   if(result % 2 == 0) {
+     return "even";
+   }
+   else {
+     return "odd";
+   }
+ }
