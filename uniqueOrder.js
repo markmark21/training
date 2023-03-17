c2713ee902fd31d1ffb801b414bf7dbb6fd7e191
Mark Khramov <hramov.mark321@gmail.com>
2023-03-17 20:40:38 +0300

    Create uniqueOrder.js

---
+++ uniqueOrder.js
@@ -0,0 +1,9 @@
+var uniqueInOrder=function(iterable){
+    let newArr = [];
+    for(let i = 0; i < iterable.length; i++) {
+      if(iterable[i] != iterable[i+1]) {
+        newArr.push(iterable[i])
+      }
+    }
+    return newArr;
+  }
