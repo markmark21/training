880b6295c7a89adb1c2633b2808b026eb11704ef
Mark Khramov <hramov.mark321@gmail.com>
2023-03-25 18:18:08 +0300

    Update somOfTwo.js

--- somOfTwo.js
+++ somOfTwo.js
@@ -14,3 +14,13 @@ function twoSum(numbers, target) {
   function reverseList(list) {
     return list.reverse()
   }
+
+  // Count The Digit
+
+  function nbDig(n, d) {
+    let result = '';
+    for(let i = 0; i <= n; i++) {
+      result += i ** 2;
+    }
+    return result.split(d).length - 1
+  }
